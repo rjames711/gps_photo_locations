d0e3bce668e3f5813d49fb0c4ec7f2e715ab0566
Rob <amesrobert2@gmail.com>
2019-06-09 08:52:53 -0400

    script now writes out to a pickle file to try to reduce processing on subsequent runs

--- get_picture_locations.py
+++ get_picture_locations.py
@@ -3,7 +3,7 @@ import os
 import geopy.distance
 from GPSPhoto import gpsphoto
 import pyexifinfo as pe
-
+import pickle
 '''
 
 location = 'boulderCo'
@@ -65,46 +65,68 @@ def traverse():
 
 
 closepaths = []
-
+file_coords = {}
 from PIL import Image
 def getWorkPicture():
+    try:
+        file_coords = pickle.load(open("coords.p","rb"))
+    except:
+        file_coords ={}
     traverse()
+    clearFiles()
+    i=0
+    for afile in paths:
+        i+=1
+        if afile in file_coords:
+            coord = file_coords[afile]
+            if get_distance(coord, coordinates) < withinKM:
+                writeFile(afile)
+        else:
+            try:
+                coord = getFileCoord(afile)
+                file_coords[afile] = coord
+                #feh below
+                if i%100 == 0:
+                    print('pickling')
+                    pickle.dump(file_coords, open("coords.p","wb"))
+                if get_distance(coord, coordinates) < withinKM:
+                    writeFile(afile)
+            except:
+                continue
+    pickle.dump(file_coords, open("coords.p","wb"))
+
+def clearFiles():
     f = open(location+str(withinKM)+'.csv', 'w')
     h = open(location+'-'+str(withinKM)+'km.html', 'w')
     f.close()
     h.close()
-    for afile in paths:
-        name = afile.split('/')
-        name = name[len(name)-1]
-        name = './thumbnails/'+name
-        print(name)
 
-        try:
-            coord = getFileCoord(afile)
-            if get_distance(coord, coordinates) < withinKM:
-                    f = open(location+'-'+str(withinKM)+'km.csv', 'a')
-                    h = open(location+'-'+str(withinKM)+'km.html', 'a')
-                    ext = afile.split('.')
-                    ext = ext[len(ext)-1]
-                    if ext == 'mp4':
-                        html = '<video width="320" height="240" controls><source src="'+afile+'"type="video/mp4"></video>'
-                    else:
-                        
-                        im = Image.open(afile)
-                        size = 128, 128
-                        im.thumbnail(size)
-                        im.save(name)
-                        html = '<a href="'+afile+'" >'
-                        html += '<img src="'+name+'" >'
-                        html += '</a>'
-                    afile += '\n'
-                    f.write(afile)
-                    h.write(html)
-                    h.close()
-                    f.close()
-                    closepaths.append(afile)
-        except:
-            continue
+def writeFile(afile):
+    name = afile.split('/')
+    name = name[len(name)-1]
+    name = './thumbnails/'+name
+    f = open(location+'-'+str(withinKM)+'km.csv', 'a')
+    h = open(location+'-'+str(withinKM)+'km.html', 'a')
+    ext = afile.split('.')
+    ext = ext[len(ext)-1]
+    if ext == 'mp4':
+        html = '<video width="320" height="240" controls><source src="'+afile+'"type="video/mp4"></video>'
+    else:
+        
+        im = Image.open(afile)
+        size = 128, 128
+        im.thumbnail(size)
+        im.save(name)
+        html = '<a href="'+afile+'" >'
+        html += '<img src="'+name+'" >'
+        html += '</a>'
+    afile += '\n'
+    f.write(afile)
+    h.write(html)
+    h.close()
+    f.close()
+    closepaths.append(afile)
+
 
 if __name__=="__main__":
     getWorkPicture()
